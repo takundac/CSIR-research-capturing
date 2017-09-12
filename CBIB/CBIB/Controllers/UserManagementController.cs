21239963358ec93820b77fafd8decd864cfebac0
takundac <tkchikondo@gmail.com>
2017-09-12 18:25:59 +0200

    Added assignment of roles to users

--- CBIB/CBIB/Controllers/UserManagementController.cs
+++ CBIB/CBIB/Controllers/UserManagementController.cs
@@ -1,30 +1,84 @@
 ﻿using CBIB.Data;
+using CBIB.Models;
 using CBIB.Views.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace CBIB.Controllers
 {
     public class UserManagementController : Controller
     {
-        private readonly ApplicationDbContext _dbcontext;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserManagementController(ApplicationDbContext dbcontext)
+        public UserManagementController(
+            ApplicationDbContext dbContext,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
-            _dbcontext = dbcontext;
+            _dbContext = dbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+
             var vm = new UserManagementIndexViewModel
             {
-                Users = _dbcontext.Users.OrderBy(u => u.Email).ToList()
+                Users = _dbContext.Users.OrderBy(u => u.Email).Include(u => u.Roles).ToList()
+            };
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddRole(string id)
+        {
+            var user = await GetUserById(id);
+            var vm = new UserManagementAddRoleViewModel
+            {
+                Roles = GetAllRoles(),
+                UserId = id,
+                Email = user.Email
             };
             return View(vm);
         }
-        
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(UserManagementAddRoleViewModel rvm)
+        {
+            var user = await GetUserById(rvm.UserId);
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.AddToRoleAsync(user, rvm.NewRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+            }
+            rvm.Email = user.Email;
+            rvm.Roles = GetAllRoles();
+            return View(rvm);
+
+        }
+
+        private async Task<ApplicationUser> GetUserById(string id) =>
+            await _userManager.FindByIdAsync(id);
+
+        private SelectList GetAllRoles() => new SelectList(_roleManager.Roles.OrderBy(r => r.Name));
+
     }
 }
---
+++ CBIB/CBIB/Views/ViewModels/UserManagementAddRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CBIB.Views.ViewModels
+{
+    public class UserManagementAddRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string NewRole { get; set; }
+        public SelectList Roles { get; set; }
+        public string Email { get; set; }
+    }
+}
